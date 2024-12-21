d1f86b030e606f15fe7ecce80479cfa3becdc06b
Vasili Gulevich <vasili.gulevich@xored.com>
2024-12-21 15:55:15 +0400

    Startup script is independent from the working directory now #88

--- hugo.sh
+++ hugo.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker run --rm -p1313:1313 -ti --mount type=bind,source=/Users/vasiligulevich/git/rcptt,destination=/mnt --env HUGO_MINIFY_TDEWOLFF_HTML_KEEPCOMMENTS=true --env HUGO_ENABLEMISSINGTRANSLATIONPLACEHOLDERS=true --workdir /mnt eclipsecbi/hugo_extended:0.110.0 hugo "$@"
+# To host website on http://localhost:1313/rcptt/ , run:
+# ./hugo.sh server --bind 0.0.0.0
+
+ROOT=$(dirname $0)
+docker run --rm -p1313:1313 -ti --mount type=bind,source=${ROOT},destination=/mnt --env HUGO_MINIFY_TDEWOLFF_HTML_KEEPCOMMENTS=true --env HUGO_ENABLEMISSINGTRANSLATIONPLACEHOLDERS=true --workdir /mnt eclipsecbi/hugo_extended:0.110.0 hugo "$@"
